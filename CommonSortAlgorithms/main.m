35201c96246095f4a752ce7ae640e734027e118d
Matt Hawkins <matthew.b.hawkins@gmail.com>
2013-07-14 15:12:02 -0400

    Initial Commit

---
+++ CommonSortAlgorithms/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CommonSortAlgorithms
+//
+//  Created by Matt Hawkins on 7/14/13.
+//  Copyright (c) 2013 Matt Hawkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
